f848931a80ca2c4e190af400c75a788ffee869c4
terukouki07 <azumacha08@gmail.com>
2023-12-27 09:36:45 +0000

    finish[routes]

--- config/routes.rb
+++ config/routes.rb
@@ -1,60 +1,58 @@
 Rails.application.routes.draw do
+#public側
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+    
+    resources :items, only: [:index, :show]
+    
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    
+    resources :cart_items, only: [:index, :update, :create, :destroy] do
+      collection do
+        delete 'destroy_all' => 'cart_items#destroy_all'
+      end
+    end
+    
+    resources :orders, only: [:new, :create, :index, :show] do
+      collection do
+        post 'confirm' => 'orders#confirm'
+        get 'thanks' => 'orders#thanks'
+      end
+    end
+    
+    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+  end
+  
+#admin側
   namespace :admin do
-    get 'orders/show'
+    root to: 'homes#top'
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    resources :order_details, only: [:update]
   end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  get 'cart_items/index'
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+
 # 顧客用
 # URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+#adminのdeviseのルーティングであることを表す。devise_for :admin
+#adminのdevisenのコントローラであるpasswordsコントローラのルーティングを作成しない記述。skip: [:passwords]
