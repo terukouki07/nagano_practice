d7fe9d943e195ac08d3d2d674b74a0d57fc24631
terukouki07 <azumacha08@gmail.com>
2023-12-28 01:01:17 +0000

    finish[genre]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,29 @@
 class Admin::ItemsController < ApplicationController
-  def index
-  end
-
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
+  end
+  
+  def index
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :genre_id, :introduction, :is_active, :price)
   end
 end
