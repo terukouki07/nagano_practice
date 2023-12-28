3f50719a98f8a191fcf145882ef40e8148ea123c
terukouki07 <azumacha08@gmail.com>
2023-12-28 00:07:47 +0000

    finish[address]

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,43 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to request.referer
+    else
+      @addresses = Address.all
+      render :index
+    end
   end
 
   def edit
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.customer_id = current_customer.id
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @address.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :address, :postal_code, :customer_id)
   end
 end
